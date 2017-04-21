58ecdcae16fa42034db9f69919e66b5a64a5519d
dbbdementev <dbbdementev@rambler.ru>
2017-04-22 00:34:16 +0700

    keys

--- bot-master/flask_app.py
+++ bot-master/flask_app.py
@@ -17,14 +17,17 @@ def processing():
     data = json.loads(request.data)
     if 'type' not in data.keys():
         return 'not vk'
-    if data['type'] == 'confirmation':
-        return confirmation_token
-    elif data['type'] == 'message_new':
-        messageHandler.create_answer(data['object'], token)
-        return 'ok'
-    elif data['type'] == 'group_join':
-        messageHandler.create_new_user(data['object'], token)
-        return 'ok'
-    elif data['type'] == 'group_leave':
-        messageHandler.create_delete_user(data['object'], token)
-        return 'ok'
+    if data['secret'] in token:
+        if data['type'] == 'confirmation':
+            return confirmation_token[data['secret']]
+        elif data['type'] == 'message_new':
+            messageHandler.create_answer(data['object'], token[data['secret']])
+            return 'ok'
+        elif data['type'] == 'group_join':
+            messageHandler.create_new_user(data['object'], token[data['secret']])
+            return 'ok'
+        elif data['type'] == 'group_leave':
+            messageHandler.create_delete_user(data['object'], token[data['secret']])
+            return 'ok'
+    else:
+        return 'not secret'
